ace9ce4e8daf058e048b22b8f7f228258ca9e05d
GeorgeOduor <george.wamaya@gmail.com>
2024-01-13 13:21:53 +0300

    latest

--- R/testR.R
+++ R/testR.R
@@ -1,11 +1,15 @@
-
 library(googlesheets4)
 df = data.frame(matrix(rnorm(20), nrow=10))
-df
+options(
+  gargle_oauth_cache = "./.secrets/",
+  gargle_oauth_email = "george.wamaya@gmail.com"
+  )
+gs4_auth()
 (ss <- gs4_create("Sample R", sheets = df,))
-gs4_find("Sample R") %>%
-  googledrive::drive_trash()
+
 for (i in 1:10) {
   sheet_append(ss,data = df)
   Sys.sleep(10)
 }
+gs4_find("Sample R") %>%
+  googledrive::drive_trash()
